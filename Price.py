96a0b32ae9495eefbee5ad2b983fb9d1becc0f6b
SWhite-Horse <56865928+SWhite-Horse@users.noreply.github.com>
2021-01-30 22:55:53 +0800

    Game

--- Price.py
+++ Price.py
@@ -1,1 +1,19 @@
-print("sad")
+import random
+from typing import Any, Union
+
+print("Welcome to my game!")
+x = int(input("请输入：（剪刀 0，石头 1，布 2)\n"))
+y = random.randint(0, 2)
+if y == 0:
+    print("电脑出剪刀\n")
+elif y == 1:
+    print("电脑出石头\n")
+else:
+    print("电脑出布\n")
+if x == y:
+    print("平局，请重新开始：")
+elif x - y == 1 or x - y == -2:
+    print("Win!!!!!")
+else:
+    print("You lose!")
+
---
+++ price_2.py
@@ -0,0 +1,5 @@
+#-*- coding = utf08 -*-
+#@Time : 2021/1/30 20:27
+#@Author : SWhite-Horse
+#@File : price_2.py
+#@Software: PyCharm
