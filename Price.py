26f34bedb9bcd17827fd1f8c651aa446cc8ca242
SWhite-Horse <56865928+SWhite-Horse@users.noreply.github.com>
2021-02-01 21:14:20 +0800

    Dict

--- Price.py
+++ Price.py
@@ -1,23 +1,5 @@
-import random
-To_Do = [["虞姬", 888], ["貂蝉", 1888], ["克拉拉", 18888], ["林志玲", 88888], ["迪丽热巴", 99999]]
-for Did in To_Do:
-    x = To_Do.index(Did)
-    print(x+1, end='\t')
-    for i in To_Do[x]:
-        print(i, end='\t')
-    print('\n')
-Did_Choose = []
-num = 0
-Pay = 0
-while num < 5:
-    Did_Choose.append(int(input("请问你要干辣锅：")))
-    if Did_Choose[num] == 9:
-        Did_Choose.pop()
-        break
-    Pay += To_Do[Did_Choose[num]-1][1]
+To_Do = {"虞姬": 888, "貂蝉": 1888, "克拉拉": 18888, "林志玲": 88888, "迪丽热巴": 99999} #@字典
+print(To_Do.get("克拉", 22))   #@第二个参数是默认值，找不到就是这个值
+To_Do["傻子"] = 9999999  #@字典的访问是【】，舔改也是【】
+#@clear 和 del 有区别，后者直接删除了字典，无法访问
 
-    num += 1
-print("你想嫖", end='')
-for i in Did_Choose:
-    print(To_Do[i-1][0], end=",")
-print("一共要花：%d"%Pay)
