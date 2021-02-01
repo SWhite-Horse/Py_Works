08e4e3fd626ca97657be85809b91eb90b31916c6
SWhite-Horse <56865928+SWhite-Horse@users.noreply.github.com>
2021-02-01 13:37:01 +0800

    Mul

--- Price.py
+++ Price.py
@@ -1,19 +1,14 @@
 import random
-from typing import Any, Union
+for i in range(1,10,1):
+    for j in range(1,10,1):
+        if j <= i:
+            print("%d*%d=%d"%(i, j, i*j), end="\t")
+            if i == j:
+                print('\n')
 
-print("Welcome to my game!")
-x = int(input("请输入：（剪刀 0，石头 1，布 2)\n"))
-y = random.randint(0, 2)
-if y == 0:
-    print("电脑出剪刀\n")
-elif y == 1:
-    print("电脑出石头\n")
-else:
-    print("电脑出布\n")
-if x == y:
-    print("平局，请重新开始：")
-elif x - y == 1 or x - y == -2:
-    print("Win!!!!!")
-else:
-    print("You lose!")
+a = [1, 2]
+b = [3, 4]
+a.extend(b)
+a.insert(4, 3)
+print(a, '\n', b)
 
