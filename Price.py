05321d33d5ff19a0158b0570d9cdaf15f2375dcc
SWhite-Horse <56865928+SWhite-Horse@users.noreply.github.com>
2021-02-01 13:57:37 +0800

    Distribution

--- Price.py
+++ Price.py
@@ -1,14 +1,7 @@
 import random
-for i in range(1,10,1):
-    for j in range(1,10,1):
-        if j <= i:
-            print("%d*%d=%d"%(i, j, i*j), end="\t")
-            if i == j:
-                print('\n')
-
-a = [1, 2]
-b = [3, 4]
-a.extend(b)
-a.insert(4, 3)
-print(a, '\n', b)
-
+office = [[], [], []]
+Name = ["A", "B", "C", "D", "E", "F", "G"]
+for i in range(len(Name)):
+    x = random.randint(0, 2)
+    office[x].append(Name[i])
+print(office)
